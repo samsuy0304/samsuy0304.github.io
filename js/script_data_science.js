6ae86423dea357fdd6b24f20d3edb214db9369a2
Suyash Sakhare <71359041+samsuy0304@users.noreply.github.com>
2024-01-12 15:53:01 -0600

    Pushing for david to see

---
+++ js/script_data_science.js
@@ -0,0 +1,12 @@
+// java_about.js
+
+// $('.navbar a').on('click',function(e)){
+//     if(this.hash !=''){
+//         e.preventDefault();
+//         const has = this.hash;
+//         $('html,body').animate({scrollTop: $(hash).offset().scrollTop})
+//         800
+//     }
+// }
+
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {speed:800});
